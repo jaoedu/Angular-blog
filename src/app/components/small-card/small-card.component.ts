45d40a98d3dd372df31a27f7c29a658d875217ae
João Mota <ejoao282@gmail.com>
2023-09-25 12:16:08 -0300

    components dinamycs

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent {
-
+  @Input()
+  cardTitle: string = "";
+  @Input()
+  photoCover: string = "";
+  @Input()
+  link: string = "";
 }
